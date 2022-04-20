84a75b4f547aa170eb2bc343a181f9a33fdf8451
Srinivas1130 <srinivasachar1998@gmail.com>
2022-04-20 08:34:11 +0530

    Gambler start with the stake 00 every day and  every game

---
+++ GamblerUC1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.workshop;
+
+public class GamblerUC1 {
+	
+        static final int stakePerDay=100;
+        static final int betPerGame=1;
+        public static void main (String[] args) {
+        	System.out.println("Gambler stake per day $"+stakePerDay);
+        	System.out.println("Gambler bet per day $"+betPerGame);
+        	}
+}
